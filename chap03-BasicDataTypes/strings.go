bb8ae616fe43d7e326a08b5cea8302a3ed26b57e
Peter Walker <peterwalker0695@gmail.com>
2023-03-08 14:39:33 +0530

    strings

--- chap03-BasicDataTypes/strings.go
+++ chap03-BasicDataTypes/strings.go
@@ -2,3 +2,16 @@ package main
 
 import "fmt"
 
+func main() {
+	var s string = "Hello, World"
+	fmt.Println(s[:5], s[7:])
+	fmt.Println("Alert is \\a \a, backspace is \\b s\b, form feed is \\f \f, tab is \\t \t, vertical tab is \\v \v")
+
+	// raw string
+	fmt.Println(`Alert is \\a \a, backspace is \\b s\b, form feed is \\f \f, tab is \\t \t, vertical tab is \\v \v`)
+
+	const GoBook = ` The Go Programming language
+	Alan and Kerninghan
+	`
+	fmt.Println(GoBook)
+}
