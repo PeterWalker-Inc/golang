8c8b89ad73bf7c31b939dfc81dbdf43f2ad8b26a
Peter Walker <peterwalker0695@gmail.com>
2023-03-09 12:07:48 +0530

    strings

--- chap03-BasicDataTypes/strings.go
+++ chap03-BasicDataTypes/strings.go
@@ -10,8 +10,28 @@ func main() {
 	// raw string
 	fmt.Println(`Alert is \\a \a, backspace is \\b s\b, form feed is \\f \f, tab is \\t \t, vertical tab is \\v \v`)
 
-	const GoBook = ` The Go Programming language
-	Alan and Kerninghan
-	`
-	fmt.Println(GoBook)
+	fmt.Println(HasPrefix(s, "Hello"))
+	fmt.Println(HasSuffix(s, "World"))
+	fmt.Println(Contains(s, "orl"))
+
+	// Hex
+	fmt.Printf("Hex of பீட்டர்: % x\n", "பீட்டர்")
+	fmt.Println(string(65))
+}
+
+func Contains(s, substr string) bool {
+	for i := 0; i < len(s); i++ {
+		if HasPrefix(s[i:], substr) {
+			return true
+		}
+	}
+	return false
+}
+
+func HasPrefix(s, prefix string) bool {
+	return len(s) >= len(prefix) && s[:len(prefix)] == prefix
+}
+
+func HasSuffix(s, suffix string) bool {
+	return len(s) >= len(suffix) && s[len(s)-len(suffix):] == suffix
 }
